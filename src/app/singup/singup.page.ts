701fb9aa65faabc3e0d88a7b26a55d8ee3a07804
AyeshaMustansar <choimoi594@gmail.com>
2026-07-21 09:45:22 +0500

    done chages in  the html and css of the sinup page   first apply template then chage the templae in to the reactive   form , alsoadd string password  7 chracter 1 digit 1 upper case , 1 speial character

--- src/app/singup/singup.page.ts
+++ src/app/singup/singup.page.ts
@@ -1,10 +1,10 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { addIcons } from 'ionicons';
-import { add, eyeOffOutline, fingerPrintOutline, happyOutline, logoFacebook, logoGoogle, mailOutline } from 'ionicons/icons';
+import { add, eyeOffOutline, eyeOutline, fingerPrintOutline, happyOutline, logoFacebook, logoGoogle, mailOutline } from 'ionicons/icons';
 
 @Component({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -12,53 +12,113 @@ import { add, eyeOffOutline, fingerPrintOutline, happyOutline, logoFacebook, log
   templateUrl: './singup.page.html',
   styleUrls: ['./singup.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, ReactiveFormsModule]
 })
 export class SingupPage implements OnInit {
-
-  email: string = '';
-  password: string = '';
-
+   
   storedEmail: string = 'choimoi594@gmail.com';
-  storedPassword: string = '12345678';
+  storedPassword: string = 'A123456789@';
 
   showAlert: boolean = false;
   alertMessage: string = '';
   alertType: string = '';
 
+  failedAttempts: number = 0;
   isButtonDisabled: boolean = false;
+  countdown: number = 10;
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, this.gmailValidator]),
+    password: new FormControl('', [Validators.required, this.strongPasswordValidator])
+  });
 
   constructor(private router: Router) {
     addIcons({
       add, mailOutline, eyeOffOutline, logoGoogle,
-      logoFacebook, happyOutline, fingerPrintOutline,
+      logoFacebook, happyOutline, fingerPrintOutline, eyeOutline,
     });
   }
 
   ngOnInit() {
   }
 
+  gmailValidator(control: AbstractControl): ValidationErrors | null {
+    const value = control.value || '';
+    return value.includes('@gmail.com') ? null : { notGmail: true };
+  }
+
+  strongPasswordValidator(control: AbstractControl): ValidationErrors | null {
+    const value = control.value || '';
+    const minLength = value.length >= 7;
+    const hasUpperCase = /[A-Z]/.test(value);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+    const hasDigit = /[0-9]/.test(value);
+
+    const valid = minLength && hasUpperCase && hasSpecialChar && hasDigit;
+    return valid ? null : { weakPassword: true };
+  }
+
+  get hasMinLength(): boolean {
+    return (this.loginForm.get('password')?.value || '').length >= 7;
+  }
+
+  get hasUpperCase(): boolean {
+    return /[A-Z]/.test(this.loginForm.get('password')?.value || '');
+  }
+
+  get hasSpecialChar(): boolean {
+    return /[!@#$%^&*(),.?":{}|<>]/.test(this.loginForm.get('password')?.value || '');
+  }
+
+  get hasDigitChar(): boolean {
+    return /[0-9]/.test(this.loginForm.get('password')?.value || '');
+  }
+
   onLogin() {
-    if (this.isButtonDisabled) {
+    if (this.isButtonDisabled || this.loginForm.invalid) {
       return;
     }
 
-    if (this.email === this.storedEmail && this.password === this.storedPassword) {
+    const email = this.loginForm.value.email;
+    const password = this.loginForm.value.password;
+
+    if (email === this.storedEmail && password === this.storedPassword) {
       this.alertMessage = 'Login Successful';
       this.alertType = 'success';
+      this.failedAttempts = 0;
     } else {
-      if (this.email !== this.storedEmail) {
+      this.failedAttempts++;
+
+      if (email !== this.storedEmail) {
         this.alertMessage = 'Invalid Email';
       } else {
         this.alertMessage = 'Invalid Password';
       }
       this.alertType = 'error';
 
-
-      this.isButtonDisabled = true;
+      if (this.failedAttempts >= 3) {
+        this.startLockout();
+      }
     }
 
     this.showAlert = true;
+  }
+
+  startLockout() {
+    this.isButtonDisabled = true;
+    this.countdown = 10;
+    this.alertMessage = 'Too many attempts. Try again in 10 seconds';
+    this.alertType = 'error';
+
+    const timer = setInterval(() => {
+      this.countdown--;
+
+      if (this.countdown <= 0) {
+        clearInterval(timer);
+        this.isButtonDisabled = false;
+        this.failedAttempts = 0;
+      }
+    }, 1000);
   }
 
   closeAlert() {
@@ -81,4 +141,6 @@ export class SingupPage implements OnInit {
     this.router.navigate(['/security-fingerprint']);
   }
 
-}
+} 
+
+
